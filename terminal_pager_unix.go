c1a7d8627dfb2f0957547f97b14a0136cd4afede
feeeper <shirobokov.andrey@gmail.com>
2018-10-10 23:28:38 +0400

    Revert "Add platform-based constants for `less` and `more` commands."
    This reverts commit a04be582eea4f17a3fe5443d4622dad4e00898f1.

--- terminal_pager_unix.go
+++ terminal_pager_unix.go
@@ -3,4 +3,3 @@
 package main
 
 const PATH_TO_TERMINAL_PAGER_PROGRAM = "/usr/bin/less"
-const PARAMS_FOR_TERMINAL_PAGER_PROGRAM = "-s"
--- terminal_pager_windows.go
+++ terminal_pager_windows.go
@@ -3,4 +3,3 @@
 package main
 
 const PATH_TO_TERMINAL_PAGER_PROGRAM = "more"
-const PARAMS_FOR_TERMINAL_PAGER_PROGRAM = "/S"
